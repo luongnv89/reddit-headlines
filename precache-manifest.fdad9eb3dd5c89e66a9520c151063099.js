e3e82c84eb26fb9ef8fcef6f27a3169b1543a5f5
Luong NGUYEN <luongnv89@gmail.com>
2019-07-26 17:33:38 +0200

    add first version

---
+++ precache-manifest.fdad9eb3dd5c89e66a9520c151063099.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58d796389d315ccce98707618375f1d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "53c5901be6d59519768c",
+    "url": "./static/js/2.c0f748e8.chunk.js"
+  },
+  {
+    "revision": "65e73f146ce902aab88a",
+    "url": "./static/js/main.a35fcbc5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
